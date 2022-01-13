f1a0f9c9d2656b736ed0e5f1bd343edcf9db3822
Sanjay Krishna S <sanjay.krishna@mail.ffhcare.net>
2022-01-13 12:32:25 +0530

    Basic Ui setup

---
+++ app/src/main/java/com/sanjay/instaclone/ui/feed/FeedFragment.kt
@@ -0,0 +1,38 @@
+package com.sanjay.instaclone.ui.feed
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.sanjay.instaclone.R
+
+class FeedFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = FeedFragment()
+    }
+
+    private lateinit var viewModel: FeedViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val feed = arguments?.getString("feed")
+        val rootView = inflater.inflate(R.layout.fragment_feed, container, false)
+
+        feed?.let {
+            rootView.findViewById<TextView>(R.id.tvFeedType).text = it
+        }
+
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(FeedViewModel::class.java)
+    }
+}
---
+++ app/src/main/java/com/sanjay/instaclone/ui/feed/FeedViewModel.kt
@@ -0,0 +1,7 @@
+package com.sanjay.instaclone.ui.feed
+
+import androidx.lifecycle.ViewModel
+
+class FeedViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
